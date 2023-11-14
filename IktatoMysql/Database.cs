1d25d8994bda0ae71766d7764d91d758ae0b59bc
KulcsarValter <kulcsarvalter1975@gmail.com>
2023-11-14 18:57:36 +0100

    Add project files.

---
+++ IktatoMysql/Database.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace IktatoMysql
+{
+    class Database
+    {
+        static readonly string server = "127.0.0.1",user = "root", password = "", database = "iktato";
+        public static string connection_string = $"Server ='{server}'; user ='{user}'; database ='{database}';password ='{password}'";
+
+        public MySqlConnection mySqlConnection = new MySqlConnection(connection_string);
+
+        public Database()
+        {
+
+        }
+
+        public bool connect_db()
+        {
+            try
+            {
+                mySqlConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool open_db()
+        {
+            try
+            {
+                mySqlConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool close_db()
+        {
+            try
+            {
+                mySqlConnection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        
+    }
+}
---
+++ IktatoMysql/Mainform.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace IktatoMysql
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.iktatóKönyvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.levelezésiTémákToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iktatásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iktatásTallozóToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.személyekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.osztályokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beosztásokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kimutatásokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.címzettekLeveleiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.levelekDátumSzerintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.levelekSzűréseNaptárralToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iktatóKönyvToolStripMenuItem,
+            this.személyekToolStripMenuItem,
+            this.osztályokToolStripMenuItem,
+            this.beosztásokToolStripMenuItem,
+            this.kimutatásokToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // iktatóKönyvToolStripMenuItem
+            // 
+            this.iktatóKönyvToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.levelezésiTémákToolStripMenuItem,
+            this.iktatásToolStripMenuItem,
+            this.iktatásTallozóToolStripMenuItem});
+            this.iktatóKönyvToolStripMenuItem.Name = "iktatóKönyvToolStripMenuItem";
+            this.iktatóKönyvToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.iktatóKönyvToolStripMenuItem.Text = "Iktató könyv";
+            // 
+            // levelezésiTémákToolStripMenuItem
+            // 
+            this.levelezésiTémákToolStripMenuItem.Name = "levelezésiTémákToolStripMenuItem";
+            this.levelezésiTémákToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.levelezésiTémákToolStripMenuItem.Text = "Levelezési témák";
+            // 
+            // iktatásToolStripMenuItem
+            // 
+            this.iktatásToolStripMenuItem.Name = "iktatásToolStripMenuItem";
+            this.iktatásToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.iktatásToolStripMenuItem.Text = "Iktatás";
+            // 
+            // iktatásTallozóToolStripMenuItem
+            // 
+            this.iktatásTallozóToolStripMenuItem.Name = "iktatásTallozóToolStripMenuItem";
+            this.iktatásTallozóToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.iktatásTallozóToolStripMenuItem.Text = "Iktatás tallozó";
+            // 
+            // személyekToolStripMenuItem
+            // 
+            this.személyekToolStripMenuItem.Name = "személyekToolStripMenuItem";
+            this.személyekToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.személyekToolStripMenuItem.Text = "Személyek";
+            // 
+            // osztályokToolStripMenuItem
+            // 
+            this.osztályokToolStripMenuItem.Name = "osztályokToolStripMenuItem";
+            this.osztályokToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.osztályokToolStripMenuItem.Text = "Osztályok";
+            // 
+            // beosztásokToolStripMenuItem
+            // 
+            this.beosztásokToolStripMenuItem.Name = "beosztásokToolStripMenuItem";
+            this.beosztásokToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.beosztásokToolStripMenuItem.Text = "Beosztások";
+            // 
+            // kimutatásokToolStripMenuItem
+            // 
+            this.kimutatásokToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.címzettekLeveleiToolStripMenuItem,
+            this.levelekDátumSzerintToolStripMenuItem,
+            this.levelekSzűréseNaptárralToolStripMenuItem});
+            this.kimutatásokToolStripMenuItem.Name = "kimutatásokToolStripMenuItem";
+            this.kimutatásokToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.kimutatásokToolStripMenuItem.Text = "Kimutatások";
+            // 
+            // címzettekLeveleiToolStripMenuItem
+            // 
+            this.címzettekLeveleiToolStripMenuItem.Name = "címzettekLeveleiToolStripMenuItem";
+            this.címzettekLeveleiToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.címzettekLeveleiToolStripMenuItem.Text = "Címzettek levelei";
+            // 
+            // levelekDátumSzerintToolStripMenuItem
+            // 
+            this.levelekDátumSzerintToolStripMenuItem.Name = "levelekDátumSzerintToolStripMenuItem";
+            this.levelekDátumSzerintToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.levelekDátumSzerintToolStripMenuItem.Text = "Levelek szűrése iktatási dátum szerint";
+            // 
+            // levelekSzűréseNaptárralToolStripMenuItem
+            // 
+            this.levelekSzűréseNaptárralToolStripMenuItem.Name = "levelekSzűréseNaptárralToolStripMenuItem";
+            this.levelekSzűréseNaptárralToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.levelekSzűréseNaptárralToolStripMenuItem.Text = "Levelek szűrése naptárral";
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Mainform";
+            this.Text = "Iktatás Mysql Adatbázissal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem iktatóKönyvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem levelezésiTémákToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iktatásToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iktatásTallozóToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem személyekToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem osztályokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beosztásokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kimutatásokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem címzettekLeveleiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem levelekDátumSzerintToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem levelekSzűréseNaptárralToolStripMenuItem;
+    }
+}
+
